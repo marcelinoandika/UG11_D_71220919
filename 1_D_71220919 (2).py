621a565a1a3161643600ef0516e474f99085cf00
marcelinoandika <117901348+marcelinoandika@users.noreply.github.com>
2022-12-01 15:50:40 +0700

    Update 1_D_71220919 (2).py

---
+++ 1_D_71220919 (2).py
@@ -0,0 +1,32 @@
+# fungsi pemilihan bioskop
+print("Dimanakah kamu ingin menonton?")
+print("1) XXI Empire")
+print("2) XXI Amplaz")
+print("3) XXI JCM")
+pilihan=int(input("masukkan pilihanmu: "))
+
+# fungsi pemilihan film
+print("Mau nonton film apa nih? Ada film:")
+print("1. Frozen")
+print("2. Keramat")
+print("3. KKN di Desa Penari")
+pilihan=int(input("Pilih nomor film: "))
+
+# fungsi pemilihan tipe layar
+print("Mau nonton layar bioskop apa: ")
+print("1. Reguler")
+print("2. Dolby Atmos")
+print("3. Premiere")
+pilihan=int(input("Pilih nomor tipe layar: "))
+
+# berapa banyak tiket 
+pilihan=int(input("Berapa banyak tiket? "))
+
+# fungsi waktu nonton
+print("Mau nonton jam berapa: ")
+print("1. 12.35")
+print("2. 14.45")
+print("3. 16.55")
+print("4. 19.05")
+pilihan=int(input("Masukkan angka pilihan anda: "))
+print("Oke berhasil, silahkan dinikmati di jam 12.35")
